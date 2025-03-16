292d48c94f556aee7241e250645d305aa205e399
Alexandr Kochurov <b2b4ly@gmail.com>
2025-03-16 19:24:22 +0100

    feat: break ice

--- Assets/Player/Scripts/PlayerAttackController.cs
+++ Assets/Player/Scripts/PlayerAttackController.cs
@@ -3,13 +3,31 @@ using UnityEngine;
 public class PlayerAttackController : MonoBehaviour
 {
     public BreakableIce breakableIce;
+    public LayerMask iceBlockLayers;
+    public Transform attackPoint;
+    
+    public float attackRange = 1f;
+    public int attackDamage = 40;
 
     public void Attack()
     {
-        if (breakableIce != null)
+
+        // detect targets in range of attack
+        Collider2D[] hitTargets =  Physics2D.OverlapCircleAll(attackPoint.position, attackRange, iceBlockLayers);
+
+        // damage them
+        foreach (Collider2D target in hitTargets)
         {
-            Debug.Log("Attack triggered!");
-            breakableIce.Attack();
+            Debug.Log("We hit: " + target.name);
+            target.GetComponent<BreakableIce>().TakeDamage(attackDamage);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
--- Assets/Scripts/BreakableIce.cs
+++ Assets/Scripts/BreakableIce.cs
@@ -1,29 +1,58 @@
+using System;
 using UnityEngine;
 
 public class BreakableIce : MonoBehaviour
 {
-    public GameObject breakEffect;
+    public int maxHealth = 100;
+    int currentHealth = 100;
+    
+    public Sprite fullIceSprite;  
+    public Sprite crackedIceSprite; 
+    public Sprite brokenIceSprite; 
+    
+    private SpriteRenderer spriteRenderer;
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void Start()
     {
-        Debug.Log("Collided with: " + other.gameObject.name);
-        if (other.CompareTag("PlayerAttack"))
+        currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateSprite();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateSprite();
+        
+        // play hurt animation
+        if (currentHealth <= 0)
         {
-            DestroyIce();
+            BreakIce();
+        }
+    }
+    
+    void UpdateSprite()
+    {
+        if (currentHealth > maxHealth * 0.66f) // more them 66% health
+        {
+            spriteRenderer.sprite = fullIceSprite;
+        }
+        else if (currentHealth > maxHealth * 0.33f) // from 33% to 66% health
+        {
+            spriteRenderer.sprite = crackedIceSprite;
+        }
+        else // less then 33% health
+        {
+            spriteRenderer.sprite = brokenIceSprite;
         }
     }
 
-    public void Attack()
+    void BreakIce()
     {
-        DestroyIce();
-    }
-
-    void DestroyIce()
-    {
-        if (breakEffect != null)
-        {
-            Instantiate(breakEffect, transform.position, Quaternion.identity);
-        }
+        Debug.Log("Ice has broken");
+        // die animation?
+        
+        // destroy the ice block
         Destroy(gameObject);
     }
 }
